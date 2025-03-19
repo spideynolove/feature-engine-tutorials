019b5b3c7b2f41500c13050fc6ba58decad8e317
spideynolove <spideynolove@gmail.com>
2025-03-19 08:40:01 +0700

    Add utility function to load Titanic dataset and update feature selection notebook

---
+++ utils.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+def load_titanic(filepath='titanic.csv'):
+    data = pd.read_csv(filepath)
+    data = data.replace('?', np.nan)
+    data['cabin'] = data['cabin'].astype(str).str[0]
+    data['pclass'] = data['pclass'].astype('O')
+    data['age'] = data['age'].astype('float').fillna(data.age.median())
+    data['fare'] = data['fare'].astype('float').fillna(data.fare.median())
+    data['embarked'].fillna('C', inplace=True)
+    return data
